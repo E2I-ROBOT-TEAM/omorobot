40eeaaa806558e6811e11f2edb4a9e925ce5a530
kimken424 <142002610+kimken424@users.noreply.github.com>
2025-02-14 20:11:23 +0900

    Update mode.py

--- Communication/mode.py
+++ Communication/mode.py
@@ -2,8 +2,8 @@ import socket
 import serial
 
 
-ip = '192.168.1.222'
-port = 1234
+ip = '192.168.1.222' #오모로봇 포트
+port = 1234 
 
 server_socket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 server_socket.bind((ip,port))
@@ -14,7 +14,7 @@ while(1):
     data, address = server_socket.recvfrom(1024)
     ser.write(data)
     print(data)
-    if (data == b'1111'):
+    if (data == b'1111'): #데이터로 1111을 보내면 정지
         break
 
 server_socket.close()
