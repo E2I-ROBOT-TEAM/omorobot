bb8f4c9f097bfe8c2b27ffbfd1a9b2a05dab8966
kimken424 <142002610+kimken424@users.noreply.github.com>
2025-02-19 23:00:55 +0900

    Update pc_udp_client.py

--- Communication/pc_udp_client.py
+++ Communication/pc_udp_client.py
@@ -11,6 +11,9 @@ client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 message=b'$cVW,100,100\r\n'
 client_socket.sendto(message,(ip,port))
 
+modifiedMessage, serverAddress = client_socket.recvfrom (2048)
+print(modifiedMessage)
+
 def close():
     client_socket.close()
     print("연결종료")
